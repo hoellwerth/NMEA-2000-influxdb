46811fbec9c8dddd2e5ecd31266db05846a74f7f
Johannes Höllwerth <johannes.hoellwerth@protonmail.com>
2023-05-21 00:18:31 +0200

    Added InfluxDB Write

--- NMEA-2000-influxdb.ino
+++ NMEA-2000-influxdb.ino
@@ -1,9 +1,69 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
 
+#include <InfluxDbClient.h>
+
+// Wifi credentials
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define INFLUXDB_URL "influxdb-url"
+#define INFLUXDB_TOKEN "toked-id"
+#define INFLUXDB_ORG "org"
+#define INFLUXDB_BUCKET "bucket"
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN);
+
+Point sensor("wifi_status");
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect WiFi
+  Serial.println("Connecting to WiFi");
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+
+  // Add constant tags - only once
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("rssi", WiFi.RSSI());
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
 
+  // Wait 10s
+  Serial.println("Wait 10s");
+  delay(10000);
 }
